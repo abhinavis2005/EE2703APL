1deee74dc29d9290de88918049e85685ccb2642c
Abhinav I S <abhinavis2005@gmail.com>
2024-10-13 06:42:12 +0000

    assignment 5 done

--- ass5/main.py
+++ ass5/main.py
@@ -4,7 +4,13 @@ from matplotlib.patches import Rectangle
 import matplotlib.colors as mcolors
 import qwerty_layout
 import random
+from matplotlib.animation import FuncAnimation
+
 def gradual_decay(distance, radius):
+    """
+    an algebraic function that decays as (1-x/r^3) inside 
+    a circle of radius r, helps in creating the decay effect for the heatmap
+    """
     return (1 - (distance / radius) ** 3) * (distance <= radius)
 
 
@@ -60,7 +66,7 @@ def genFreq(inpStr: str, layout, characters: dict) -> tuple[np.array, np.array]:
     x, y = list(), list()
 
     for char in inpStr:
-        keys = characters[char]
+        keys = characters[char] #keys to be pressed
         for key in keys:  # for each key to be pressed
             if key == "Space":  # we need to ignore space, as it skews the heatmap
                 continue
@@ -76,6 +82,7 @@ def plot1(
     x: np.array,
     y: np.array,
     grid_size: tuple,
+    ax
 ):
     """
     Function generates plot without animation
@@ -104,7 +111,7 @@ def plot1(
     colors = ["blue", "green", "yellow", "orange", "red"]
     blue_red = mcolors.LinearSegmentedColormap.from_list("blue_red", colors)
 
-    plt.imshow(
+    ax.imshow(
         heatmap.T,
         cmap=blue_red,
         interpolation="gaussian",
@@ -121,7 +128,7 @@ def caculate_key_travel(input_string: str, layout: dict, keys: dict):
     This function calculates the key travel, taking in the input string, the layout
     dictionary and the keys dictionary
     """
-    sum = 0
+    sum = 0 
     for c in input_string:
         for key in keys[c]:  # iterating for each key to be pressed
             if key == "Space": #ignoring space  
@@ -145,27 +152,41 @@ def layout_update(keys:dict) -> dict:
     return keys
 
 def neighbour_solution(keys:dict) -> dict:
+    """
+    generates a neighbour layout to the given layout
+    for simulated annealing, by sampling from the 
+    keys of the dictionary, excluding the special 
+    characters
+    We dont want solutions where shift, tab etc go 
+    to the middle of they keyboard """
     new_keys = keys.copy()
-    i, j = random.sample(list(keys.keys()),2)
+    # dont swap special keys
+    
+    i, j = random.sample(list(keys.keys())[:-7],2)
     #only keys themselves change, not pos or homerow key
     new_keys[i], new_keys[j] = new_keys[j], new_keys[i] 
     return new_keys
 
-def simulated_annealing(layout, initial_temp, cooling_rate, num_itreations, inpString):
+def simulated_annealing(layout:dict, initial_temp:float, cooling_rate:float, num_itreations:int, inpString:str)->tuple[list, list, dict]:
+    
+    temp = initial_temp #initial conditions
     current_distance = caculate_key_travel(inpString, layout.keys, layout.characters)
-
     current_layout= layout.keys.copy()
+    
+    #best conditions
     best_layout = current_layout
     best_distance = current_distance
 
-    temp = initial_temp
     distances = [current_distance]
     best_distances = [best_distance]
 
-    for i in range(num_itreations):
+    for _ in range(num_itreations):
         neighbour_layout = neighbour_solution(current_layout)
         neighbour_distance = caculate_key_travel(inpString, neighbour_layout, layout.characters)
-        p = np.exp((current_distance - neighbour_distance)/ temp)
+        arg = (current_distance - neighbour_distance)/ temp
+        max_val = 700
+
+        p = np.exp(np.clip(arg, None, 700))# probability
 
         if neighbour_distance < current_distance or random.random() < p:
             current_distance = neighbour_distance
@@ -179,33 +200,55 @@ def simulated_annealing(layout, initial_temp, cooling_rate, num_itreations, inpS
         distances.append(current_distance)
         best_distances.append(best_distance)
 
-        return best_distances, distances
+    return best_distances, distances, best_layout
+
+def update(frame, best_distances, distances, best_distance_line, distance_line):
+    distance_line.set_data(range(frame+1), distances[:frame+1])
+    best_distance_line.set_data(range(frame+1), best_distances[:frame+1])
+    return distance_line, best_distance_line
+
+
 if __name__ == "__main__":
 
-    fig, ax = plt.subplots(figsize=(14, 8))  # generating axes , fig object
-    # setting limits for axes
+    fig, ax1 = plt.subplots(1,1, figsize=(14, 8))  # generating axes , fig object
+   
+    layout = qwerty_layout
+
+
+    inputStr = input("Enter string : ")
+    layout_update(layout.keys) #changing the layout to be compatible for simulated annealing
+
+    #hyperparameters
+    initial_temp = 1000
+    cooling_rate = 0.98
+    num_iterations = 1000
+    best_distances, distances, best_layout = simulated_annealing(layout, initial_temp, cooling_rate, num_iterations, inputStr)
+
+    #  simulated annealing plot
+    ax1.set_title("Simulated Annealing Distance vs Iterations")
+    ax1.set_xlim(0, num_iterations)
+    ax1.set_ylim(min(best_distances)*0.9, max(distances)*1.1)
+    distance_line, = ax1.plot([],[], 'r-')
+    best_distance_line, = ax1.plot([],[], 'g-')
+
+   
+    #generating animation
+    anim = FuncAnimation(fig, update, frames = range(0, num_iterations,15), fargs=(best_distances, distances, best_distance_line, distance_line), 
+                         interval =0, blit=True, repeat=False)
+    anim.save('animation.gif', writer='pillow', fps=30)
+
+    #heatmap generation
+    grid_size = (58, 16)  # Number of pixels in x and y`
+    fig1, ax = plt.subplots(1, 1, figsize=(14,8))
+    ax.set_title("Optimized Keyboard Heatmap")
     ax.set_xlim(-2, 16)
     ax.set_ylim(-1, 6)
     ax.set_aspect("equal")
     ax.axis("off")
-    layout = qwerty_layout
-    # generating thhe keyboard layout
-    genKeyboardLayout(ax, layout.keys)
+    genKeyboardLayout(ax, best_layout)
+    x, y = genFreq(inputStr, best_layout, layout.characters)
+    plot1(x, y, grid_size, ax)
+    fig1.savefig('heatmap.png', bbox_inches='tight')
 
-    grid_size = (58, 16)  # Number of pixels in x and y`
-
-    inputStr = input("Enter string : ")
-    layout_update(layout.keys)
-    initial_temp = 100
-    cooling_rate = 0.995
-    num_iterations = 100
-    best_distances, distances = simulated_annealing(qwerty_layout, initial_temp, cooling_rate, num_iterations, inputStr)
-
-    fig, ax1 = plt.subplots(figsize=(15,6))
-    ax1.set_title("Best Distances over iterations")
-
-    distance_line = ax1.plot(best_distances, 'r-')
-    cur_dist_line = ax1.plot(distances, 'g-')
-    plt.show()
-
-    print(f"Total distance travelled is {best_distances[-1]} units")
+    
+    print(f"Total distance travelled after optimization is {best_distances[-1]} units")
